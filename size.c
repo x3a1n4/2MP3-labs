1975d6588cab0a764ee3e25bf1065c45cb47d8b8
Alex <frasea24@mcmaster.ca>
2023-09-27 11:43:57 -0400

    Update size.c

--- size.c
+++ size.c
@@ -1,2 +1,17 @@
 // created file
 // added second comment
+
+#include <stdio.h>
+
+int main(){
+    char charArray[] = {'A','B','C','D','E'};
+    int intArray[] = {1,2,3,4,5};
+
+    for(int i = 0; i < sizeof(charArray); i++){
+        printf("Element %d: %p\n", i, charArray[i]);
+    }
+
+    for(int i = 0; i < sizeof(intArray); i++){
+        printf("Element %d: %p\n", i, intArray[i]);
+    }
+}
